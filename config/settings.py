1f9f401a810cbf32500a3d46806f240b23e12bbf
unknown <you@example.com>
2025-06-19 06:29:39 -0700

    fix: added Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ['armaniestate.onrender.com']
 
 
 # Application definition
